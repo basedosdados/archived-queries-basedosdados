f04ff20b2bfb6e7c8b9afb160578127dd0414780
d116626 <d116626@gmail.com>
2022-05-17 19:06:23 +0000

    Deploying to gh-pages from @ basedosdados/queries@7073e6c3383d6a4540f02f6002558806a52b12f6 🚀

---
+++ compiled/basedosdados/models/example/my_second_dbt_model.sql
@@ -0,0 +1,5 @@
+-- Use the `ref` function to select from other models
+
+select *
+from `basedosdados-dev`.`example`.`my_first_dbt_model`
+where id = 1
---
+++ compiled/basedosdados/models/example/schema.yml/schema_test/not_null_my_second_dbt_model_id.sql
@@ -0,0 +1,9 @@
+
+    
+    
+
+select *
+from `basedosdados-dev`.`example`.`my_second_dbt_model`
+where id is null
+
+
