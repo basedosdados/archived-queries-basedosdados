642f445437dadbd453cfc945a47cdcd69a21e413
folhesgabriel <pisagabriel09@gmail.com>
2023-05-09 08:47:38 -0300

    fix: models

--- models/br_me_comex_stat/municipio_exportacao.sql
+++ models/br_me_comex_stat/municipio_exportacao.sql
@@ -1,5 +1,3 @@
-
-
 SELECT 
 SAFE_CAST(ano AS INT64) ano,
 SAFE_CAST(mes AS INT64) mes,
@@ -9,4 +7,4 @@ SAFE_CAST(sigla_uf AS STRING) sigla_uf,
 SAFE_CAST(id_municipio AS STRING) id_municipio,
 SAFE_CAST(peso_liquido_kg AS INT64) peso_liquido_kg,
 SAFE_CAST(valor_fob_dolar AS INT64) valor_fob_dolar
-FROM basedosdados-dev.br_me_comex_stat_staging.municipio_exportacao AS t
+FROM basedosdados-staging.br_me_comex_stat_staging.municipio_exportacao AS t
--- models/br_me_comex_stat/ncm_importacao.sql
+++ models/br_me_comex_stat/ncm_importacao.sql
@@ -10,4 +10,4 @@ SAFE_CAST(id_urf AS STRING) id_urf,
 SAFE_CAST(quantidade_estatistica AS INT64) quantidade_estatistica,
 SAFE_CAST(peso_liquido_kg AS INT64) peso_liquido_kg,
 SAFE_CAST(valor_fob_dolar AS INT64) valor_fob_dolar
-FROM basedosdados-dev.br_me_comex_stat_staging.ncm_importacao AS t
+FROM basedosdados-staging.br_me_comex_stat_staging.ncm_importacao AS t
