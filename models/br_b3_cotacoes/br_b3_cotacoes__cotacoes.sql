e4fd409c18c65b578a8294a55db5d09a07641d5b
tricktx <patrick.tx@hotmail.com.br>
2023-05-29 13:35:32 -0300

    feat: model dbt br_b3_cotacoes prod

---
+++ models/br_b3_cotacoes/br_b3_cotacoes__cotacoes.sql
@@ -0,0 +1,16 @@
+{{ config(alias='cotacoes', schema='br_b3_cotacoes') }}
+
+SELECT 
+SAFE_CAST(data_referencia AS DATE) data_referencia,
+SAFE_CAST(tipo_sessao_pregao AS INT64) tipo_sessao_pregao,
+SAFE_CAST(codigo_instrumento AS STRING) codigo_instrumento,
+SAFE_CAST(acao_atualizacao AS INT64) acao_atualizacao,
+SAFE_CAST(data_negocio AS DATE) data_negocio,
+SAFE_CAST(codigo_identificador_negocio AS STRING) codigo_identificador_negocio,
+SAFE_CAST(preco_negocio AS FLOAT64) preco_negocio,
+SAFE_CAST(quantidade_negociada AS INT64) quantidade_negociada,
+SAFE_CAST(hora_fechamento AS INT64) hora_fechamento,
+SAFE_CAST(codigo_participante_comprador AS STRING) codigo_participante_comprador,
+SAFE_CAST(codigo_participante_vendedor AS STRING) codigo_participante_vendedor
+
+FROM basedosdados-staging.br_b3_cotacoes_staging.cotacoes AS t
